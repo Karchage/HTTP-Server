8a91aaa7c53a19e4dfe29a426701c3f36d546e21
Karchage <glebkul2010@yandex.ru>
2019-07-11 20:22:12 +0300

    Add Cliend and connection client to server

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
+int main(int argc, char* argv[])
+{
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1); //дает версию 
+	if (WSAStartup(DLLVersion, &wsaData) != 0) //Грузим библ
+	{
+		std::cout << "Escape, error: lib not load" << std::endl;
+		exit(1);
+	}
+	else std::cout << "Lib connected !!!" << std::endl;
+
+	SOCKADDR_IN adrSock; //хранение адреса
+	static const char LOCAL_HOST[] = "127.0.0.1";
+	int sizeAddr = sizeof(adrSock);
+	//adrSock.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	inet_pton(AF_INET, LOCAL_HOST, &(adrSock.sin_addr));
+	adrSock.sin_port = htons(8015);
+	adrSock.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&adrSock, sizeof(adrSock)) != 0)
+	{
+		std::cout << "Error : Connection fail";
+		return 1;
+	}
+	std::cout << "Conected !!";
+	char wmsg[64];
+	recv(Connection, wmsg, sizeof(wmsg), NULL); //get msg fr serv
+	std::cout << wmsg;
+
+	system("pause");
+	return 0;
+
+}
--- httpServer/httpServer/httpServer.cpp
+++ httpServer/httpServer/httpServer.cpp
@@ -1,7 +1,8 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <WinSock2.h>
-
+#include <Ws2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
 int main(int argc, char* argv[])
 {
 	WSAData wsaData;
@@ -11,16 +12,18 @@ int main(int argc, char* argv[])
 		std::cout << "Escape, error: lib not load" << std::endl;
 		exit(1);
 	}
+	else std::cout << "Lib connected !!!" << std::endl;
 
 	SOCKADDR_IN adrSock; //хранение адреса
+	static const char LOCAL_HOST[] = "127.0.0.1";
 	int sizeAddr = sizeof(adrSock);
-	adrSock.sin_addr.s_addr = inet_addr("127.0.0.1"); //inet_addr("128.0.0.1")
+	//adrSock.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	inet_pton(AF_INET, LOCAL_HOST, &(adrSock.sin_addr));
 	adrSock.sin_port = htons(8015);
-	adrSock.sin_family = AF_INET; // хз что тут надо 
-								  //AF_INET6
+	adrSock.sin_family = AF_INET;
 
 	SOCKET socnasok = socket(AF_INET, SOCK_STREAM, NULL);
-	bind(socnasok, (SOCKADDR*)&adrSock,sizeof(adrSock));
+	bind(socnasok, (SOCKADDR*)&adrSock,sizeof(adrSock)); //назначение
 	listen(socnasok, SOMAXCONN); // второе значение хау мач запрос 
 
 	SOCKET newUserConnection;
@@ -29,6 +32,13 @@ int main(int argc, char* argv[])
 	if (newUserConnection == 0) // проверка коннект
 	{
 		std::cout << "Error connection" << std::endl;
+	}
+	else
+	{
+		std::cout << "Client connected !" << std::endl;
+		char wmsg[64] = "Welcome to http-server \n";
+		send(newUserConnection, wmsg, sizeof(wmsg), NULL); //  SEND Welcome msg
+		
 	}
 
 	return 0;
