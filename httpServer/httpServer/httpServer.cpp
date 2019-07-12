d377007046e13798f64f116c43ff2fd093de4029
Karchage <glebkul2010@yandex.ru>
2019-07-12 16:26:10 +0300

    Create more connection

--- httpServer/httpServer/httpServer.cpp
+++ httpServer/httpServer/httpServer.cpp
@@ -6,6 +6,7 @@
 #pragma comment (lib, "Ws2_32.lib")
 int main(int argc, char* argv[])
 {
+	SOCKET connectionsUser[128];
 	WSAData wsaData;
 	WORD Version = MAKEWORD(2, 1); //дает версию 
 	if(WSAStartup(Version, &wsaData) != 0) //Грузим библ
@@ -31,84 +32,89 @@ int main(int argc, char* argv[])
 	bind(lsock, (SOCKADDR*)&adrSock,sizeof(adrSock)); //назначение
 	listen(lsock, SOMAXCONN); // второе значение скок запрос 
 
-	SOCKET newUserConnection;
-	newUserConnection = accept(lsock, (SOCKADDR*)&adrSock, &sizeAddr);
-	if (newUserConnection == 0) // проверка коннект
+	for (int i = 0; i < 128; i++)
 	{
-		std::cout << "Error connection" << std::endl;
-	}
-	else
-	{
-		std::cout << "Client connected !" << std::endl;
-		char wmsg[64] = "Welcome to http-server \n";
-		send(newUserConnection, wmsg, sizeof(wmsg), NULL); //  SEND Welcome msg
-		
-	}
-	//==========================================================================================
-
-
-	//signal code
-	const int trueFile = 200;
-	const int badFile = 404;
-
-	char filename[FILENAME_MAX];
-	const int bufer=1024;
-	char bufferFile[bufer];
-	bool closeConnection = false;
-	std::ifstream file;
-
-	do
-	{
-		//memset(filename, 0, FILENAME_MAX);
-		int byRecv = recv(newUserConnection, filename, FILENAME_MAX, NULL); //Имя файла
-		if (byRecv == 0 || byRecv == -1)
+		SOCKET newUserConnection;
+		newUserConnection = accept(lsock, (SOCKADDR*)&adrSock, &sizeAddr);
+		if (newUserConnection == 0) // проверка коннект
 		{
-			closeConnection = true;
+			std::cout << "Error connection" << std::endl;
 		}
-
-		file.open(filename, std::ios::binary); //Открываем поток файла в бинарке
-		if (file.is_open())
+		else
 		{
-			int sendInf = send(newUserConnection, (char*)&trueFile, sizeof(int), NULL); //нахождение файла сделать если не найден прерывать
-			if (sendInf == 0 || sendInf == -1)
+			std::cout << "Client connected !" << std::endl;
+			char wmsg[64] = "Welcome to http-server \n";
+			connectionsUser[i] = newUserConnection;
+			send(newUserConnection, wmsg, sizeof(wmsg), NULL); //  SEND Welcome msg
+
+		}
+		//==========================================================================================
+
+
+		//signal code
+		const int trueFile = 200;
+		const int badFile = 404;
+
+		char filename[FILENAME_MAX];
+		const int bufer = 1024;
+		char bufferFile[bufer];
+		bool closeConnection = false;
+		std::ifstream file;
+
+		do
+		{
+			//memset(filename, 0, FILENAME_MAX);
+			int byRecv = recv(newUserConnection, filename, FILENAME_MAX, NULL); //Имя файла
+			if (byRecv == 0 || byRecv == -1)
 			{
 				closeConnection = true;
 			}
 
-			file.seekg(0, std::ios::end); // Размер файла
-			long fileSize = file.tellg(); // Текущая позиция =  размер файла
-			sendInf = send(newUserConnection, (char*)&fileSize, sizeof(long), NULL); // Тож проверку запилить
-			if (sendInf == 0 || sendInf == -1)
+			file.open(filename, std::ios::binary); //Открываем поток файла в бинарке
+			if (file.is_open())
 			{
-				closeConnection = true;
-			}
-			file.seekg(0, std::ios::beg); // Сдвигаем на начало
-			do
-			{
-				file.read(bufferFile, bufer);
-				if (file.gcount() > 0)
-				{
-					sendInf = send(newUserConnection, bufferFile, file.gcount(), NULL);
-				}
+				int sendInf = send(newUserConnection, (char*)&trueFile, sizeof(int), NULL); //нахождение файла сделать если не найден прерывать
 				if (sendInf == 0 || sendInf == -1)
 				{
 					closeConnection = true;
-					break;
 				}
-				
-			} while (file.gcount() > 0);
-			file.close();
-		} 
-		else
-		{
-			int sendInf = send(newUserConnection, (char*)&badFile, sizeof(int), NULL);
-			if (sendInf == 0 || sendInf == -1)
-			{
-				closeConnection = true;
-			}
-		}
 
-	} while (!closeConnection);
+				file.seekg(0, std::ios::end); // Размер файла
+				long fileSize = file.tellg(); // Текущая позиция =  размер файла
+				sendInf = send(newUserConnection, (char*)&fileSize, sizeof(long), NULL); // Тож проверку запилить
+				if (sendInf == 0 || sendInf == -1)
+				{
+					closeConnection = true;
+				}
+				file.seekg(0, std::ios::beg); // Сдвигаем на начало
+				do
+				{
+					file.read(bufferFile, bufer);
+					if (file.gcount() > 0)
+					{
+						sendInf = send(newUserConnection, bufferFile, file.gcount(), NULL);
+					}
+					if (sendInf == 0 || sendInf == -1)
+					{
+						closeConnection = true;
+						break;
+					}
+
+				} while (file.gcount() > 0);
+				file.close();
+			}
+			else
+			{
+				int sendInf = send(newUserConnection, (char*)&badFile, sizeof(int), NULL);
+				if (sendInf == 0 || sendInf == -1)
+				{
+					closeConnection = true;
+				}
+			}
+
+		} while (!closeConnection);
+	}
+	
 
 	return 0;
 }
